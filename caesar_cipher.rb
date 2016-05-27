ff570481c5c01b9520c3dcb008ccd99b6da63694
Michael Sotkin <michael.sotkin@gmail.com>
2016-05-27 14:57:07 -0700

    allowed caesar cipher to handle larger shift factor

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,16 +1,17 @@
 
 def caesar_cipher(input, shift) 
 	encryption = ""
+	shift_large = shift.to_i % 26
 	input.scan(/./) do |x| #Search for all of the characters in input
 		x.each_byte do |c| #Change characters into ASCII
 			if c >= 65 && c <= 90 #Make sure characters wrap from z to a, staying in the same case
-				c = c + shift.to_i
+				c = c + shift_large
 				if c > 90
 					c =- 26
 				end
 				encryption << c
 			elsif c >= 97 && c <= 122
-				c = c + shift.to_i
+				c = c + shift_large
 				if c > 122
 					c =- 26
 				end
